1bc6bd9aa6fe862e2b8f1d1330b04c1d56ffb5e1
Aishwaryasai04 <aishwaryasai44@gmail.com>
2025-05-06 13:05:05 -0500

    Day 1: Python bio card program

---
+++ day1/day1_basics.py
@@ -0,0 +1,11 @@
+name= input("Enter Your name: ")
+age= input("Enter Your age: ")
+hobby= input("What is your favorite hobby? ")
+
+age_in_years = int(age)
+age_in_months= age_in_years * 12
+
+print("Hi, I'm", name + "!")
+print("I'm", age_in_years, "Years old , which is ", age_in_months,"months.")
+print("I love", hobby,  ".")
+
